4a1d0965f55e9a9ec39b7a34e60a74568933ee0c
Thinh Pham <phamvanthinh2001x@gmail.com>
2021-11-21 16:29:08 +0700

    make modal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Modal, { ModalContent } from "../components/modal/Modal";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <ModalContent children="modal content" />
+    </div>
+  );
 };
 
 export default Home;
