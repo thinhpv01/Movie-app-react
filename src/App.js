3e1dee1756d5579c2eaac55fe5421ab943f270a5
Thinh Pham <phamvanthinh2001x@gmail.com>
2021-11-21 10:14:15 +0700

    route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.scss';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import Routes from './config/Routes';
 
 function App() {
   return (
-    <div className="App">
-      a
-    </div>
+    <BrowserRouter>
+      <Route render={props => (
+        <>
+          <Header {...props} />
+          <Routes />
+          <Footer />
+        </>
+      )} />
+    </BrowserRouter>
   );
 }
 
